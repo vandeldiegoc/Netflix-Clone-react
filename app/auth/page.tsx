25c69ec64879a96ad0514f3839dc5547752bfba2
diego <vandeldiegoc@gmail.com>
2023-08-29 00:22:58 -0500

    start proyect

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+const Auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('../public/images/hero.jpg')] bg-no-repeat bg-center bg-fixes bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50 "></div>
+      <nav>
+        
+      </nav>
+    </div>
+  );
+};
+
+export default Auth;
